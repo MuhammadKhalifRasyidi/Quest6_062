2c5047c7c751b4250b98f2fa4e9d4dbd94049d21
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-11-22 11:59:09 +0700

    membuat modifier di fun MahasiswaFormView

--- app/src/main/java/com/example/meet08/ui/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/meet08/ui/screen/MahasiswaFormView.kt
@@ -3,10 +3,14 @@ package com.example.pertemuan8.ui.screen
 
 import androidx.compose.runtime.Composable
 
+import androidx.compose.ui.Modifier
+
 
 @Composable
 fun MahasiswaFormView(
-
+    modifier: Modifier,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
 ) {
 
 
