a4240c8570b14bdfcf41e46c21051015c348e9df
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-11-22 11:52:42 +0700

    membuat class MahasiswaViewModel

---
+++ app/src/main/java/com/example/meet08/ui/viewmodel/MahasiswaViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.meet08.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.meet08.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+
+
+}
