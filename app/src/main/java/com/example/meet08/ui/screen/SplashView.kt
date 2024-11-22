1a9428da6271b14022c3c471b1cfaaa9bea91842
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-11-22 15:47:33 +0700

    membuat File SplashView

---
+++ app/src/main/java/com/example/meet08/ui/screen/SplashView.kt
@@ -0,0 +1,11 @@
+package com.example.meet08.ui.screen
+
+import androidx.compose.runtime.Composable
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun SplashView() {
+
+}
