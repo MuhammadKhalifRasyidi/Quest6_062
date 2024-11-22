a023479bf9dfd4caec4dad2d43d690bfde947f4b
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-11-22 11:54:22 +0700

    membuat val mahasiswaStateUI di class MahasiswaViewModel

--- app/src/main/java/com/example/meet08/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/meet08/ui/viewmodel/MahasiswaViewModel.kt
@@ -5,12 +5,15 @@ import com.example.meet08.model.Mahasiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 
 class MahasiswaViewModel : ViewModel() {
 
     private val _mahasiswaStateUI =
         MutableStateFlow(Mahasiswa())
+
+    val mahasiswaStateUI:
+            StateFlow<Mahasiswa> =
+        _mahasiswaStateUI.asStateFlow()
 
 
 }
