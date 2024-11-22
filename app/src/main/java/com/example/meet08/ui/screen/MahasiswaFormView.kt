7ac4639915b99cb27f41e12fbaa6e8e107c75fcf
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-11-22 15:10:10 +0700

    membuat var nim, nama, email dan var listData di fun MahasiswaFormView

--- app/src/main/java/com/example/meet08/ui/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/meet08/ui/screen/MahasiswaFormView.kt
@@ -2,7 +2,10 @@ package com.example.pertemuan8.ui.screen
 
 
 import androidx.compose.runtime.Composable
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 
@@ -12,6 +15,20 @@ fun MahasiswaFormView(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
 ) {
+
+    var nim by remember {
+        mutableStateOf("")
+    }
+    var nama by remember {
+        mutableStateOf("")
+    }
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var listData: MutableList<String> =
+        mutableListOf(nim, nama, email)
+
 
 
 }
