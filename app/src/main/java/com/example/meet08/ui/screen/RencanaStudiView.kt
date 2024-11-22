639c01a2f82b3ff5ad4c0052d57d276bc478b6ae
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-11-22 15:45:11 +0700

    membuat variable di fun RencanaStudiView terdapat var chosenDropdown, checked, dan pilihanKelas

--- app/src/main/java/com/example/meet08/ui/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/meet08/ui/screen/RencanaStudiView.kt
@@ -1,7 +1,10 @@
 package com.example.meet08.ui.screen
 
 import androidx.compose.runtime.Composable
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.meet08.model.Mahasiswa
 
 @Composable
@@ -10,5 +13,11 @@ fun RencanaStudiView(
     onSumbitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
 ) {
-
+    var chosenDropdown by remember {
+        mutableStateOf("")
+    }
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember {
+        mutableStateOf("")
+    }
 }
