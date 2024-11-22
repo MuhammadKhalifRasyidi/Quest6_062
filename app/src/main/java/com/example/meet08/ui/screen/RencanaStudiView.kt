9bc9cf4ab7ddde46af792369f2cee66be0ddc4a9
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-11-22 15:43:28 +0700

    membuat File RencanaStudiView didalamnya terdapat parameter mahasiswa, onSubmitButtonClicked, onBackButtonClicked

---
+++ app/src/main/java/com/example/meet08/ui/screen/RencanaStudiView.kt
@@ -0,0 +1,14 @@
+package com.example.meet08.ui.screen
+
+import androidx.compose.runtime.Composable
+
+import com.example.meet08.model.Mahasiswa
+
+@Composable
+fun RencanaStudiView(
+    mahasiswa: Mahasiswa,
+    onSumbitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+) {
+
+}
