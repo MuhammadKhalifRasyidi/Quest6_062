016141b0d5f44fccb51d29e38001a7e1376d16e5
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-11-22 18:20:51 +0700

    update pada File SplashView ada beberapa yang diupdate

--- app/src/main/java/com/example/meet08/ui/screen/SplashView.kt
+++ app/src/main/java/com/example/meet08/ui/screen/SplashView.kt
@@ -15,13 +15,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.meet08.R
 
-@Preview(showBackground = true)
 @Composable
-fun SplashView() {
+fun SplashView(onMulaiButton: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
